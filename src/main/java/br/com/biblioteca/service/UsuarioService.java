87f2104439e4b30bd4f1171e7c033f697d1a2c15
Manuela Rios <manuela.rios.silva@gmail.com>
2025-09-14 19:50:42 -0300

    Salvando a senha criptografada no banco de dados

--- src/main/java/br/com/biblioteca/service/UsuarioService.java
+++ src/main/java/br/com/biblioteca/service/UsuarioService.java
@@ -3,15 +3,22 @@ package br.com.biblioteca.service;
 import br.com.biblioteca.model.Usuario;
 import br.com.biblioteca.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
 
     @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
     private UsuarioRepository usuarioRepository;
 
-    public Usuario salvarUsuario(Usuario usuario) {
-        return usuarioRepository.save(usuario);
+    public void salvarUsuario(Usuario usuario) {
+        String senhaCriptografada = passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(senhaCriptografada);
+
+        usuarioRepository.save(usuario);
     }
 }
